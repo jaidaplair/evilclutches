f3ef09392fdca2459c5022846c9024b1b842452d
Jaida Plair <jaiplair@gmail.com>
2024-02-09 09:46:19 -0500

    Initial commit

---
+++ Assets/BossMoving.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMoving : MonoBehaviour
+{
+    //speed of the demon moving 
+    [SerializeField] float speed = 0.005f;
+    [SerializeField] float minY = -4f;
+    [SerializeField] float maxY = 4f;
+    //the boolean travelDirection is true when we are going up and false when we are going down.
+    bool travelDirection = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (travelDirection == true )
+        {//we are moving up
+            transform.Translate(speed * Vector3.up);
+        }
+        else
+        {//we are moving down
+            transform.Translate(speed * Vector3.down);
+        }
+        //if we hit the top change direction
+        if(transform.position.y > maxY)
+        {
+            travelDirection = false;
+        }
+        //if we hit the bottom change direction
+        if(transform.position.y < minY ) 
+        { 
+            travelDirection = true;
+        }
+    }
+}
